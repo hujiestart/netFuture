3041acfafb110decc5b41c36e6bad9c60e126787
liming6 <liming6@asiainfo.com>
2019-05-02 23:35:24 +0800

    shell脚本的bug修复

--- zookeeper/vagrant/Leader/一键配置jdk/pro.sh
+++ zookeeper/vagrant/Leader/一键配置jdk/pro.sh
@@ -17,7 +17,7 @@ fi
 yum install -y wget 
 #install jdk1.8
 cd $SOFT_PATH
-wget wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz"
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz"
 tar -zxvf jdk* -C $SOFT_PATH
 cd jdk*
 JAVA_HOME=`pwd` 
