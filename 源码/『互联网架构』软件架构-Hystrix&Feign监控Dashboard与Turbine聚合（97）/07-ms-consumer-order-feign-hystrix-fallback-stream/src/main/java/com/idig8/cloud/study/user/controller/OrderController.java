22ead82aa69e7153063c56002408f5ef94ffe9bc
liming6 <liming6@asiainfo.com>
2019-06-18 21:00:03 +0800

    『互联网架构』软件架构-Hystrix&Feign监控Dashboard与Turbine聚合（97）源码

---
+++ 源码/『互联网架构』软件架构-Hystrix&Feign监控Dashboard与Turbine聚合（97）/07-ms-consumer-order-feign-hystrix-fallback-stream/src/main/java/com/idig8/cloud/study/user/controller/OrderController.java
@@ -0,0 +1,20 @@
+package com.idig8.cloud.study.user.controller;
+
+import com.idig8.cloud.study.user.feign.UserFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.idig8.cloud.study.user.entity.User;
+
+@RestController
+public class OrderController {
+  @Autowired
+  private UserFeignClient userFeignClient;
+
+  @GetMapping("/user/{id}")
+  public User findById(@PathVariable Long id) {
+    return this.userFeignClient.findById(id);
+  }
+}
